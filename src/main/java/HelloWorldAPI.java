30232ba5b152e7ba574038d21b4ac7b1d3a7a600
Bill Bensing <billbensing@bills-mbp.lan>
2023-03-30 14:05:42 -0400

    inital commit with basic API

---
+++ src/main/java/HelloWorldAPI.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+public class HelloWorldAPI {
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(80), 0);
+        server.createContext("/hello", new MyHandler());
+        server.setExecutor(null);
+        server.start();
+    }
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            if (!"GET".equals(exchange.getRequestMethod())) {
+                exchange.sendResponseHeaders(405, -1); // Method Not Allowed
+                exchange.close();
+                return;
+            }
+            String response = "{\"message\": \"Hello World\"}";
+            exchange.getResponseHeaders().set("Content-Type", "application/json");
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+}
---
+++ src/test/java/HelloWorldAPITest.java
@@ -0,0 +1,43 @@
+import static org.junit.Assert.assertEquals;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import java.net.URL;
+
+import com.sun.net.httpserver.HttpServer;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class HelloWorldAPITest {
+
+    private HttpServer server;
+    private URL base;
+
+    @Before
+    public void setUp() throws Exception {
+        server = HttpServer.create(new InetSocketAddress(0), 0);
+        server.createContext("/hello", new HelloWorldAPI.MyHandler());
+        server.setExecutor(null);
+        server.start();
+        int port = server.getAddress().getPort();
+        base = new URL("http://localhost:" + port);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        server.stop(0);
+    }
+
+    @Test
+    public void testHelloWorldAPI() throws IOException {
+        HttpURLConnection conn = (HttpURLConnection) new URL(base, "/hello").openConnection();
+        conn.setRequestMethod("GET");
+        conn.connect();
+        try (InputStream in = conn.getInputStream()) {
+            byte[] response = in.readAllBytes();
+            assertEquals("{\"message\": \"Hello World\"}", new String(response));
+        }
+    }
+}
